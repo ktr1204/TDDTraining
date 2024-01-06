7137239300e73df6a7465835d839e2c28accd10d
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-06 17:24:11 +0000

    9 ran tests, no prediction, got amber

--- files/Hiker.java
+++ files/Hiker.java
@@ -5,7 +5,7 @@ public class Hiker {
         
     }
     public static String judge(String[] hand) {
-        if(hand === {"H2", "C3", "D4", "S5", "H6"}){
+        if(hand == {"H2", "C3", "D4", "S5", "H6"}){
             return "No Hand";
         }
     }
