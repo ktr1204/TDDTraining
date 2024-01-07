3ae18ac2088a2e19ee57b21e073763b38ae6ac0f
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-07 03:38:34 +0000

    20 ran tests, no prediction, got green

--- files/Hiker.java
+++ files/Hiker.java
@@ -10,10 +10,6 @@ public class Hiker {
         if(Arrays.equals(hand, bbb)){
             return "One Pair";
         }
-        String[] aaa = {"H2", "C3", "D4", "S5", "H6"};
-        if(Arrays.equals(hand, aaa)){
             return "No Hand";
-        }
-            return "Yes Hand";
     }
 }
