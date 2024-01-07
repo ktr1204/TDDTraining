92cc5cefa2fec3801d905df1e9340dd41c421688
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-07 03:50:22 +0000

    28 ran tests, no prediction, got amber

--- files/Card.java
+++ files/Card.java
@@ -1,10 +1,10 @@
-    protected enum Fruits {
+    protected enum Card {
         Banana("Okinawa"),
         Orange("Ehime"),
         Apple("Aomori");
 
       private String madein; // フィールドの定義
-      private Fruit(String madein) { // コンストラクタの定義
+      private Card(String madein) { // コンストラクタの定義
           this.madein = madein;
         }
       }
