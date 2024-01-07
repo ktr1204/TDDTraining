0335832154c66f4d7ec7d2c5b069392d59588e2f
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-07 15:43:37 +0000

    67 ran tests, no prediction, got red

--- files/JudgeManTest.java
+++ files/JudgeManTest.java
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class JudgeManTest {
 
-    @Test
-    void NoHand() {
-        JudgeMan judgeMan = new JudgeMan();
-        String[] hand = {"H2", "C3", "D4", "S5", "H6"};
-        String actual = judgeMan.judge(hand);
-        assertEquals("No Hand", actual);
-    }
+    //@Test
+    //void NoHand() {
+    //    JudgeMan judgeMan = new JudgeMan();
+    //    String[] hand = {"H2", "C3", "D4", "S5", "H6"};
+    //    String actual = judgeMan.judge(hand);
+    //    assertEquals("No Hand", actual);
+    //}
     
-    @Test
-    void OnePair() {
-        JudgeMan judgeMan = new JudgeMan();
-        String[] hand = {"H2", "C3", "D2", "S5", "H6"};
-        String actual = judgeMan.judge(hand);
-        assertEquals("One Pair", actual);
-    }
+    //@Test
+    //void OnePair() {
+    //    JudgeMan judgeMan = new JudgeMan();
+    //    String[] hand = {"H2", "C3", "D2", "S5", "H6"};
+    //    String actual = judgeMan.judge(hand);
+    //    assertEquals("One Pair", actual);
+    //}
     @Test
     void TwoPair() {
         JudgeMan judgeMan = new JudgeMan();
