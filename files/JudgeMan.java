0720650068aaf6bc1b73c623964a3ff550f5b4c0
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-07 05:07:30 +0000

    35 ran tests, no prediction, got green

--- files/JudgeMan.java
+++ files/JudgeMan.java
@@ -5,7 +5,7 @@ public class JudgeMan {
     public JudgeMan(){
         
     }
-    public static String judge(String[] hand) {{
+    public static String judge(String[] hand) {
         String[] twoPair = {"H2", "C3", "D2", "S3", "H6"};
         if(Arrays.equals(hand, twoPair)){
             return "Two Pair";
