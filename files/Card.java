5defd35a279e96c08927ed3715db66d2cbbe15b2
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-07 05:03:42 +0000

    31 ran tests, no prediction, got amber

--- files/Card.java
+++ files/Card.java
@@ -1,10 +1,6 @@
 public enum Card {
-    Banana("Okinawa"),
-    Orange("Ehime"),
-    Apple("Aomori");
-
-    private String madein; // フィールドの定義
-    private Card(String madein) { // コンストラクタの定義
-        this.madein = madein;
-    }
+    CLOVER("Okinawa"),
+    DIAMOND("Ehime"),
+    HEART("Ehime"),
+    CLUB("Aomori");
 }
