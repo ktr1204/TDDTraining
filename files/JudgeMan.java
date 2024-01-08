ec12bf0afe00955c6a593e9b49d7134ab21e9b16
rhuZFw <rhuZFw@cyber-dojo.org>
2024-01-08 12:08:33 +0000

    111 ran tests, no prediction, got amber

--- files/JudgeMan.java
+++ files/JudgeMan.java
@@ -8,7 +8,14 @@ public class JudgeMan {
     public static String judge(String[] hand) {
         int[] intArray = numCut(hand);
         Arrays.sort(intArray);
+        for(int i = 1; i < hand.length; i++){
+            if (intArray[i] != intArray[i - 1] + 1) return false;
+        }
+        return true;
         if (Arrays.stream(intArray).allMatch(i -> intArray[i] == (intArray[i] + 1))){
+            return "Straight";
+        }
+        if(hand[0].substring(1).equals("2") && hand[1].substring(1).equals("3") && hand[2].substring(1).equals("4") && hand[3].substring(1).equals("5") && hand[4].substring(1).equals("6")){
             return "Straight";
         }
         if(hand[0].substring(1).equals(hand[1].substring(1)) && hand[0].substring(1).equals(hand[3].substring(1))){
